ff27a61d248558ce49a8b53f76a2e06a3f7f3699
jhyunwoo <jhyunwoo0228@gmail.com>
2024-12-25 21:38:30 +0900

    remove console.log

--- lib/create-day-list.ts
+++ lib/create-day-list.ts
@@ -21,6 +21,5 @@ export default function createDayList(startDate: Date, endDate: Date) {
   for (let i = 0; i < 6 - endDate.getDay(); i++) {
     dayList.push({ date: addDays(endDate, i + 1), isAvailable: false })
   }
-  console.log(dayList)
   return dayList
 }
