6ce98ff58d563a27368cc8af6010d35e1b4f5d16
jhyunwoo <jhyunwoo0228@gmail.com>
2024-12-26 18:43:00 +0900

    almost done!!!!!

--- app/components/navigation-bar/add-schedule-bar/actions.ts
+++ app/components/navigation-bar/add-schedule-bar/actions.ts
@@ -6,6 +6,8 @@ import { auth } from "@/auth"
 import { and, eq, inArray } from "drizzle-orm"
 import { redirect } from "next/navigation"
 import getMeetingData from "@/lib/get-metting-data"
+import timeList from "@/app/components/drag-to-select/time-list"
+import { addDays, addHours, addMinutes } from "date-fns"
 
 export async function addScheduleAction(
   meetingId: string,
@@ -37,6 +39,8 @@ export async function addScheduleAction(
   const neverData: (typeof schedules.$inferInsert)[] = []
   const modifiableData: (typeof schedules.$inferInsert)[] = []
 
+  const emptyDate: Date[] = []
+
   for (const data of never) {
     const date = new Date(data)
     if (date >= meetingData.startDate && date <= meetingData.endDate) {
@@ -58,9 +62,35 @@ export async function addScheduleAction(
     }
   }
 
+  let indexDate = meetingData.startDate
+
+  while (indexDate <= meetingData.endDate) {
+    for (const time of timeList) {
+      const hour = Number(time.split(":")[0])
+      const minute = Number(time.split(":")[1])
+      const targetDate = addMinutes(addHours(indexDate, hour), minute)
+      if (
+        !never.includes(targetDate.toJSON()) &&
+        !modifiable.includes(targetDate.toJSON())
+      ) {
+        emptyDate.push(targetDate)
+      }
+    }
+    indexDate = addDays(indexDate, 1)
+  }
+
   if (neverData.length > 0) await db.insert(schedules).values(neverData)
   if (modifiableData.length > 0)
     await db.insert(schedules).values(modifiableData)
+  if (emptyDate.length > 0)
+    await db
+      .delete(schedules)
+      .where(
+        and(
+          eq(schedules.userId, session.user.id),
+          inArray(schedules.date, emptyDate)
+        )
+      )
 
   redirect(`/meetings/${meetingId}`)
 }
